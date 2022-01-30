2dc21905fa5f4908f6616faed6920fd0ea4569dc
Victor Lobato <victorsilva123190@gmail.com>
2022-01-30 10:07:34 -0300

    Implementando AccountController e adicionando application.properties

--- bank-api/src/main/kotlin/com/victorlobato/bankapi/Account.kt
+++ bank-api/src/main/kotlin/com/victorlobato/bankapi/Account.kt
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity(name = "accounts")
-data class Account(
+data class Account(                                                                                                                      
     @Id @GeneratedValue
     var id: Long? = null,
     val name: String,
